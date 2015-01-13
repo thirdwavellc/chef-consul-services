9b2925a5dceb0f22b6e83504f7e94253dd72a3bb
Adam Krone <krone.adam@gmail.com>
2015-01-13 16:49:17 -0600

    Changes wordpress check url to match bedrock

--- recipes/wordpress.rb
+++ recipes/wordpress.rb
@@ -12,7 +12,7 @@ include_recipe 'curl::default'
 consul_service_def 'wordpress' do
   check(
     interval: '10s',
-    script: 'curl http://localhost/wp-login.php | grep Username || exit 2'
+    script: 'curl http://localhost/wp/wp-login.php | grep Username || exit 2'
   )
   notifies :restart, "service[consul]", :delayed
 end
