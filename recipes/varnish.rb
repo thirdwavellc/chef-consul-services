860eb5a0400bfe1069070f26d6be8657c7beb4ea
Adam Krone <krone.adam@gmail.com>
2015-01-27 10:40:07 -0600

    Adds varnish check

---
+++ recipes/varnish.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: consul-services
+# Recipe:: varnish
+#
+# Copyright (C) 2014
+#
+#
+#
+
+consul_check_def 'varnish' do
+  port 6081
+  check(
+    interval: '10s',
+    script: 'pgrep varnish || exit 2'
+  )
+  notifies :restart, "service[consul]", :delayed
+end
