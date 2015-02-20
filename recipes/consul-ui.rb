376be5af0a933590f5f78a68b3777bdd80ad50d0
Adam Krone <krone.adam@gmail.com>
2015-02-20 14:31:13 -0600

    Adds consul-ui service

---
+++ recipes/consul-ui.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: consul-services
+# Recipe:: consul-ui
+#
+# Copyright 2014 Adam Krone <adam.krone@thirdwavellc.com>
+# Copyright 2014 Thirdwave, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+consul_service_def 'consul-ui' do
+  port 8500
+  check(
+    interval: '10s',
+    script: 'lsof -i :8500 | grep consul || exit 2'
+  )
+  notifies :restart, "service[consul]", :delayed
+end
